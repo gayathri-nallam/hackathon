5d9a1a65d82a5f9c45c5011874eb2f8b76e004b2
gayathri-nallam <55805504+gayathri-nallam@users.noreply.github.com>
2021-12-03 16:48:25 +0530

    Update request-accommodation.component.ts

--- src/app/request-accommodation/request-accommodation.component.ts
+++ src/app/request-accommodation/request-accommodation.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RequestService } from '../request.service';
 import { User } from '../user';
+import { IDropdownSettings, } from 'ng-multiselect-dropdown';
 
 @Component({
   selector: 'app-request-accommodation',
@@ -12,11 +13,23 @@ export class RequestAccommodationComponent implements OnInit {
   user:User =new User();
 
   constructor(private requestService: RequestService) { }
-
-  ngOnInit(): void {
+  dropdownList = [];
+  dropdownSettings:IDropdownSettings={};
+  ngOnInit(){
+    this.dropdownList = [
+      { item_id: 1, item_text: 'Gym' },
+      { item_id: 2, item_text: 'Club house' },
+      { item_id: 3, item_text: 'Park' },
+      { item_id: 4, item_text: 'Shopping Mall' },
+      { item_id: 5, item_text: 'Hospital' }
+    ];
+    this.dropdownSettings = {
+      idField: 'item_id',
+      textField: 'item_text',
+    };
   }
   accommadationtype : string[]=['1BHK','2BHK','PG','Sharing','Single Room']
-  amenitys : string[] =['Gym','Club House','Park']
+  //amenitys : string[] =['Gym','Club House','Park']
   userRegister(){
     console.log(this.user);
     this.requestService.registerUser(this.user).subscribe(data=>{
